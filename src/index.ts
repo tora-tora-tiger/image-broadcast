edec149fce41a35ea0c50964e3f0d890fe920d3d
tora <bmvdsfy@gmail.com>
2026-01-01 00:42:59 +0900

    static配下も配信

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ app.use(logger())
 
 app.use("/mics2.png", serveStatic({ path: './static/mics2.png' }))
 app.use("/image.jpeg", serveStatic({ path: './static/image.jpeg' }))
+app.use("/static/*", serveStatic({ root: './' }))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
